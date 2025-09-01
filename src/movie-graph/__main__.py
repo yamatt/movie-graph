08f72934ea28f43e5d02477b75584df9a4899503
Matt Copperwaite <618054+yamatt@users.noreply.github.com>
2025-09-01 21:31:21 +0100

    removing duplicate line

--- src/movie-graph/__main__.py
+++ src/movie-graph/__main__.py
@@ -7,7 +7,6 @@ import click
 
 @click.command()
 @click.argument("sqlite_file_path", default="imdb.db")
-@click.argument("sqlite_file_path", default="imdb.db")
 def convert(sqlite_file_path="imdb.db"):
     sqlite = SQLite(sqlite_file_path)
     neo4j = Neo4J.from_env()
